e4ea907be1da9b70223412bdba96d5051dd836a8
Azzuro Najma Annaila <azzuronajmaannaila@gmail.com>
2025-12-15 14:50:07 +0700

    change: api url

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -10,7 +10,8 @@ st.set_page_config(
     layout="wide",
 )
 
-API_URL = "http://127.0.0.1:5000/predict"
+API_URL = "https://abaloneproject-production.up.railway.app/predict"
+
 
 # =========================
 # I18N (ID/EN)
